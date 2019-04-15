8d8dc08992f5f587d905721de2366b0cd8aaa683
Norton M. B. Santos <norton@alunos.utfpr.edu.br>
2019-04-15 11:11:20 -0300

    Adding shoulda matchers

--- Gemfile
+++ Gemfile
@@ -20,6 +20,10 @@ group :development, :test do
   gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 end
 
+group :test do
+  gem 'shoulda-matchers'
+end
+
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe User, type: :model do
 
   let(:user){build(:user)}
   
-  context "When user is valid" do
-    it { expect(user).to respond_to(:email)  }
-    it { expect(user).to be_valid  }
-  end
+  # context "When user is valid" do
+  #   it { expect(user).to respond_to(:email)  }
+  #   it { expect(user).to be_valid  }
+  # end
   
-  context "When email is blank" do
-    before{ user.email = ''}
-    it { expect(user).to_not be_valid  }
-  end
+  # context "When email is blank" do
+  #   before{ user.email = ''}
+  #   it { expect(user).to_not be_valid  }
+  # end
   
-  context "When email is nil" do
-    before{ user.email = nil}
-    it { expect(user).to_not be_valid  }
-  end
+  # context "When email is nil" do
+  #   before{ user.email = nil}
+  #   it { expect(user).to_not be_valid  }
+  # end
 
-  # it {expect(user).to validate_presence_of(:email)}
+  it {expect(user).to validate_presence_of(:email)}
 
 end
