e09b54018608bbeb5ad86a9bc0742f2c5a7b1842
Norton M. B. Santos <norton@alunos.utfpr.edu.br>
2019-04-17 18:37:43 -0300

    Fixing the route error

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 require 'api_version_constraint'
 
-
 Rails.application.routes.draw do
 
   root to: 'application#index'
 
-  devise_for :users
-  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
-    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+  # devise_for :users
+  namespace :api, defaults: {format: :json}, path: '/' do
+    namespace :v1, path: "/" do
       resources :users, only: [:show]
     end
   end
